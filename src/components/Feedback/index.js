4c32fb6367f3643cd49dcb2dad8e584af2777915
Pavan <pn8189977@gmail.com>
2023-02-05 06:41:31 +0000

    Feedback-app

---
+++ src/components/Feedback/index.js
@@ -0,0 +1,68 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class Feedback extends Component {
+  state = {
+    isFeedbackSelected: false,
+  }
+
+  onClickEmoji = () => this.setState({isFeedbackSelected: true})
+
+  renderFeedback = () => {
+    const {resources} = this.props
+    const {emojis} = resources
+
+    return (
+      <div className="feedback-container">
+        <h1 className="feedback-question">
+          How satisfied are you with our customer support performance?
+        </h1>
+        <u1 className="emojis-list">
+          {emojis.map(emoji => (
+            <li key={emoji.id}>
+              <button
+                type="button"
+                className="emoji-btn"
+                onClick={this.onClickEmoji}
+              >
+                <img src={emoji.imageUrl} alt={emoji.name} className="emoji" />
+                <br />
+                <span className="emoji-name">{emoji.name}</span>
+              </button>
+            </li>
+          ))}
+        </u1>
+      </div>
+    )
+  }
+
+  renderThankYou = () => {
+    const {resources} = this.props
+    const {loveEmojiUrl} = resources
+
+    return (
+      <div className="thank-you-container">
+        <img src={loveEmojiUrl} alt="love emoji" className="love-emoji" />
+        <h1 className="thank-you-text">Thank You!</h1>
+        <p className="description">
+          We will use your feedback to improve our customer support performance
+        </p>
+      </div>
+    )
+  }
+
+  render() {
+    const {isFeedbackSelected} = this.state
+
+    return (
+      <div className="app-container">
+        <div className="feedback-card">
+          {isFeedbackSelected ? this.renderThankYou() : this.renderFeedback()}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Feedback
